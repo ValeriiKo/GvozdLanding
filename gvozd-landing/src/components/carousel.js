3bfb7dbf192082423a431e86e8a144a3e842f9ce
Valerii <valeriikozhevnikov@gmail.com>
2024-05-06 23:00:47 +0300

    added carousel

--- gvozd-landing/src/components/carousel.js
+++ gvozd-landing/src/components/carousel.js
@@ -3,27 +3,6 @@ import { Carousel } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS directly
 import { Image } from "./image";
 
-
-const responsive = {
-    superLargeDesktop: {
-      // the naming can be any, depends on you.
-      breakpoint: { max: 4000, min: 3000 },
-      items: 5
-    },
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 3
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 2
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1
-    }
-  };
-
 export const CarouselAuto = (props) =>{
     return (
     <Carousel pause={false} interval={3000}>
@@ -32,11 +11,21 @@ export const CarouselAuto = (props) =>{
           <Image src="../images/img1.jpg" />
           <Image src="../images/img2.jpg" />
           <Image src="../images/img3.jpg" />
+          <Image src="../images/img4.jpg" />
         </div>
       </Carousel.Item>
       <Carousel.Item>
         <div className="d-flex justify-content-center">
-        <Image src="../images/img4.jpg" />
+          <Image src="../images/img2.jpg" />
+          <Image src="../images/img3.jpg" />
+          <Image src="../images/img4.jpg" />
+          <Image src="../images/img5.jpg" />
+        </div>
+      </Carousel.Item>
+      <Carousel.Item>
+        <div className="d-flex justify-content-center">
+          <Image src="../images/img3.jpg" />
+          <Image src="../images/img4.jpg" />
           <Image src="../images/img5.jpg" />
           <Image src="../images/img6.jpg" />
         </div>
--- gvozd-landing/src/components/image.js
+++ gvozd-landing/src/components/image.js
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from '../images/img1.jpg';
 
 export const Image = ({src}) => {
   return (
